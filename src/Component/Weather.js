1cc39ade13662de5c921839add458f50093b6ea2
lincey-cherian-au7 <linceycherian@gmail.com>
2020-11-18 14:32:37 +0530

    Final

--- src/Component/Weather.js
+++ src/Component/Weather.js
@@ -5,10 +5,10 @@ import './Weather.css'
 const Weather =(props)=>{
     return(
         <div className="container">
-            <div className="card">
+            <div className="card" style={{color:'white'}}>
                 <h1>{props.city},{props.country}</h1>
                 <h5 className="py-4">
-                    <i className={`wi ${props.weatherIcon} display-3`}></i>
+                    <i className={`wi ${props.weatherIcon} display-3`} ></i>
                 </h5>
                 <h1 className="py-2">
                     {props.temp}&deg;
